b71185e2feeb8b11491faaa562b481ae9a98bf5c
Jacky <jacky@jhlab.com>
2021-01-08 21:21:22 -0500

    Update main.go

--- main.go
+++ main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
 	"github.com/jianconnect/goclone/crawler"
 	"github.com/jianconnect/goclone/file"
-	"github.com/jianconnect/goclone/parser"
 )
 
 func main() {
-	url := os.Args[1]
+	urlStr := os.Args[1]
 
-	if !parser.ValidateURL(url) && !parser.ValidateDomain(url) {
-		fmt.Println("goclone <url>")
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		fmt.Printf("goclone <url> \n Error url:%s", urlStr)
 	} else {
-		name := url
+		name := u.Hostname()
 		// CreateProject
 		projectpath := file.CreateProject(name)
 		// create the url
 		//validURL := parser.CreateURL(name)
 		// Crawler
-		crawler.Crawl(url, projectpath)
+		crawler.Crawl(urlStr, projectpath)
 	}
 
 }
